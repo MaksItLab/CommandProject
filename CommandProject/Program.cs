472c4aaa12c55a76814c19da8f6c04fd46d37ae7
Ivan <redshadow689@gmail.com>
2025-03-07 19:20:51 +0300

    Легенда на Иване создал ветвь

--- CommandProject/Program.cs
+++ CommandProject/Program.cs
@@ -10,6 +10,8 @@ namespace CommandProject
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Ivan");
+            Console.WriteLine("Legend");
         }
     }
 }
